6a50897179049a1847b309c5ac066ca13d906191
Bao. Dang <dang.hbaokody@gmail.com>
2024-10-12 04:05:12 +0700

    update 2024-10-12 04:05:12

--- container/podman.linux.sh
+++ container/podman.linux.sh
@@ -1,3 +1,9 @@
 sudo apt install podman pipx -y
 pipx install podman-compose
+
 # update docker.io in /etc/containers/registries.conf
+sudo apt install sed -y
+grep -Fxq 'unqualified-search-registries = ["docker.io"]' /etc/containers/registries.conf \
+|| sudo sed -i "1i unqualified-search-registries = [\"docker.io\"]" /etc/containers/registries.conf
+
+# update network version 1.0.0 to 0.4.0 to resolve cni error if exists

79d6261c38b56533ddfbcd8ba893498c361debde
Bao. Dang <dang.hbaokody@gmail.com>
2024-10-12 11:12:09 +0700

    update 2024-10-12 11:12:08

---
+++ tool/cmake.linux.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install cmake -y
+
+# essential packages when working with cmake
+sudo apt install build-essential -y # make, g++, g
+sudo apt install gdb -y
+
+# libraries ## TODO
+
+# generator
+sudo apt install make -y
+sudo apt install ninja-build -y
